a5dca11d8e6ea97406234785e92b970d678b7081
Veronika Ozretic <vozret00@fesb.hr>
2021-05-06 12:58:42 +0200

    first commit

---
+++ src/components/Users.js
@@ -0,0 +1,74 @@
+import React from "react";
+import User from "./User";
+
+import classes from "./Users.module.css";
+
+const DUMMY_USERS = [
+  { id: "u1", name: "Max" },
+  { id: "u2", name: "Manuel" },
+  { id: "u3", name: "Julie" },
+];
+
+class Users extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showUsers: true.name,
+      more: "Hi!",
+    };
+  }
+
+  // we have to make sure that "this" keyword inside our method reffers to the surrounding class
+  toggleUsersHandler = () => {
+    // "this" is now set to the same context or the same value
+    // as the "this" keyword when we call toggleUserHandler method
+    this.setState((prevState) => {
+      return { showUsers: !prevState.showUsers };
+    });
+  };
+
+  render() {
+    const usersList = (
+      <ul>
+        {DUMMY_USERS.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
+    return (
+      <div className={classes.users}>
+        <button onClick={this.toggleUsersHandler.bind(this)}>
+          {this.state.showUsers ? "Hide" : "Show"} Users
+        </button>
+        {this.state.showUsers && usersList}
+      </div>
+    );
+  }
+}
+
+// const Users = () => {
+//   const [showUsers, setShowUsers] = useState(true);
+
+//   const toggleUsersHandler = () => {
+//     setShowUsers((curState) => !curState);
+//   };
+
+//   const usersList = (
+//     <ul>
+//       {DUMMY_USERS.map((user) => (
+//         <User key={user.id} name={user.name} />
+//       ))}
+//     </ul>
+//   );
+
+//   return (
+//     <div className={classes.users}>
+//       <button onClick={toggleUsersHandler}>
+//         {showUsers ? "Hide" : "Show"} Users
+//       </button>
+//       {showUsers && usersList}
+//     </div>
+//   );
+// };
+
+export default Users;
